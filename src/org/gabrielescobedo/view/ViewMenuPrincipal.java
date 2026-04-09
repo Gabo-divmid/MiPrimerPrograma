997dabc1009133e693fc1d648cd3f66f3291fc15
Gabriel Escobedo <gescobedo-2023401@kinal.edu.gt>
2026-04-09 11:52:19 -0600

    001 Se agregaron las opciones al menu principal

--- src/org/gabrielescobedo/view/ViewMenuPrincipal.java
+++ src/org/gabrielescobedo/view/ViewMenuPrincipal.java
@@ -10,6 +10,10 @@ public class ViewMenuPrincipal {
        //Local
         while (opcionMenu == 0){
             System.out.println("Bienvenido, Seleccione una opcion: ");
+            System.out.println("1. Operaciones Matematicas. ");
+            System.out.println("2. Operaciones de texto.");
+            System.out.println("3. Operaciones de Logica.");
+            System.out.println("4. Salir del sistema.");
             opcionMenu = Integer.parseInt(leer.nextLine());
             
         }
