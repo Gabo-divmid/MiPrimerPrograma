adc7b13ae7da0d591cf47adb453f2c60c5bdb384
Diego Escobedo <gescobedo-2023401@kinal.edu.gt>
2026-04-12 19:01:13 -0600

    002 Se agrego ejercicio  Concatenador de nombres 

--- src/org/gabrielescobedo/controller/ControllerTexto.java
+++ src/org/gabrielescobedo/controller/ControllerTexto.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package org.gabrielescobedo.controller;
 
+package org.gabrielescobedo.controller;
+    import java.util.Scanner;
 /**
  *
  * @author informatica
  */
 public class ControllerTexto {
-    
+
+   public class ConcatenadorNombre {
+
+    public static String concatenarNombre(String nombre, String apellido) {
+        return nombre + " " + apellido;
+    }
+
+    public static void main(String[] args) {
+        String resultado = concatenarNombre("Juan", "Perez");
+        System.out.println(resultado); // Juan Perez
+    }
 }
+      
+        
